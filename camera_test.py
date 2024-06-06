5793b563ae7182ed75968556f21de85399e5afa2
Logan-Thom <155180737+Logan-Thom@users.noreply.github.com>
2024-06-06 10:26:01 +0100

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -1,8 +1,12 @@
 from time import sleep
+import datetime
 from picamera import PiCamera
 
 camera = PiCamera()
 camera.resolution=(1024,768)
 camera.start_preview()
 sleep(2)
-camera.capture("picture_test.jpg")
+
+while True:
+  camera.capture("picture_test.jpg" + str(datetime.time))
+  sleep(3)
